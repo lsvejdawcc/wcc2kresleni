3d10f1a8969847fb14d2226b67a53b17b168bde3
Libor Svejda <libor.svejda@unicorncollege.cz>
2021-11-25 18:16:39 +0100

    tloustka a barva car

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function poNacteni() {
 
     //rovne cary
     ctx.beginPath();
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "red";
     ctx.moveTo(0,0);
     ctx.lineTo(200,100);
     ctx.lineTo(200,150);
@@ -12,11 +14,15 @@ function poNacteni() {
 
     //obdelnik
     ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "green";
     ctx.rect(20, 200, 80, 40);
     ctx.stroke();
 
     //kruznice
     ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "blue";
     ctx.arc(250, 200, 30, 0, 2*Math.PI);
     ctx.stroke();
 
